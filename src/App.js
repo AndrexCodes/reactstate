f6479fd45e3d78afaaf7b841e33c073b6e7c177c
AndrexCodes <123867051+AndrexCodes@users.noreply.github.com>
2024-07-30 14:45:05 +0300

    Finalized project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    // This is the initial state
+    this.state = {
+      person: {
+        fullName: 'Andrew Macharia',
+        bio: 'A passionate software developer.',
+        imgSrc: 'https://via.placeholder.com/150',
+        profession: 'Software Engineer'
+      },
+      shows: false,
+      timeElapsed: 0
+    };
+  }
+
+  componentDidMount() {
+    this.interval = setInterval(() => {
+      this.setState(prevState => ({
+        timeElapsed: prevState.timeElapsed + 1
+      }));
+    }, 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+
+  toggleShow = () => {
+    this.setState(prevState => ({
+      shows: !prevState.shows
+    }));
+  };
+
+  render() {
+    const { person, shows, timeElapsed } = this.state;
+    return (
+      <div className="App">
+        <button onClick={this.toggleShow}>
+          {shows ? 'Hide' : 'Show'} Profile
+        </button>
+        {shows && (
+          <div className="profile">
+            <img src={person.imgSrc} alt={person.fullName} />
+            <h1>{person.fullName}</h1>
+            <p>{person.bio}</p>
+            <h2>{person.profession}</h2>
+          </div>
+        )}
+        <div className="timer">
+          Time elapsed since mount: {timeElapsed} seconds
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
